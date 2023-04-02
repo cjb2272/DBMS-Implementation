2724c5fcac348afdc17b99f85f465493937194a1
acc5989 <acc5989@g.rit.edu>
2023-04-02 17:27:14 -0400

    Implement cartesian product.

--- src/Table.java
+++ src/Table.java
@@ -1,37 +1,36 @@
 package src;
 
+import java.lang.reflect.Array;
 import java.util.ArrayList;
 
 /*
-Represents a table file on disk. Inside a table file is a series of pages containing rows of data.
-The page represents the smallest unit of memory that can be read/written at any given time.
+In-memory tables used by select to perform cartesian products
 @author: Austin Cepalia
  */
 public class Table {
 
-    // the unique ID of this table
-    private int id;
-
-    // the display name for the table
-    // (only use this for interaction with the user, AKA input and output)
     private String name;
 
-    // maps each attribute (column) name to its type
     private ArrayList<String> columnNames;
-    private ArrayList<Integer> dataTypes;
 
-    // the number of pages currently held in this table file
-    private int numPages;
+    private ArrayList<Record> records;
 
-    public Table(int id, String name, ArrayList<String> columnNames, ArrayList<Integer> dataTypes) {
-        this.id = id;
+    public Table(String name, ArrayList<String> columnNames, ArrayList<Record> records) {
         this.name = name;
         this.columnNames = columnNames;
-        this.dataTypes = dataTypes;
-        this.numPages = 0;
+        this.records = records;
+    }
+
+    public ArrayList<Record> getRecords() {
+        return records;
+    }
+
+    public ArrayList<String> getColNames() {
+        return columnNames;
     }
 
     public String getName() {
-        return this.name;
+        return name;
     }
+
 }
