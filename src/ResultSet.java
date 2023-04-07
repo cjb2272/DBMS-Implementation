53e0ceae1561f68fd0c6c83f2f6bd513d88d60b3
acc5989 <acc5989@g.rit.edu>
2023-04-07 01:57:08 -0400

    Add qualifiedColumnNames to ResultSet

--- src/ResultSet.java
+++ src/ResultSet.java
@@ -10,12 +10,38 @@ public class ResultSet {
 
     private ArrayList<String> tableNamesForColumns;
 
+    private ArrayList<String> qualifiedColumnNames;
+
     public ResultSet(ArrayList<Record> records, ArrayList<String> recordColumnNames,
                      ArrayList<Integer> recordColumnTypes, ArrayList<String> tableNamesForColumns) {
         this.records = records;
         this.columnNames = recordColumnNames;
         this.columnTypes = recordColumnTypes;
         this.tableNamesForColumns = tableNamesForColumns;
+
+        this.qualifiedColumnNames = new ArrayList<>();
+
+
+        ArrayList<Integer> duplicateIdxs = new ArrayList<>();
+
+        for (int i = 0; i < this.columnNames.size(); i++) {
+            for (int j = i+1; j < this.columnNames.size(); j++) {
+                if (this.columnNames.get(i).equals(this.columnNames.get(j)) && !duplicateIdxs.contains(j)) {
+                    duplicateIdxs.add(i);
+                    duplicateIdxs.add(j);
+                }
+            }
+        }
+
+        for (int i = 0; i < columnNames.size(); i++) {
+            if (duplicateIdxs.contains(i)) {
+                this.qualifiedColumnNames.add(this.tableNamesForColumns.get(i) + "." + this.columnNames.get(i));
+            }
+            else {
+                this.qualifiedColumnNames.add(this.columnNames.get(i));
+            }
+        }
+
     }
 
     public ArrayList<String> getColumnNames() {
@@ -31,4 +57,5 @@ public class ResultSet {
     }
 
     public ArrayList<String> getTableNamesForColumns() { return tableNamesForColumns; }
+    public ArrayList<String> getQualifiedColumnNames() { return qualifiedColumnNames; }
 }
